e5d44bc19965b32823658c4bd03bb14e912ad5e3
rakangstn-sudo <rakangstn@gmail.com>
2025-10-29 14:18:33 +0700

    Add files via upload

---
+++ tugas mandiri pak yudi.py
@@ -0,0 +1,17 @@
+for i in range (5,0,-1):    #pola angka menurun
+    for j in range (1,i+1):
+        print(j,end='')
+    print()
+print("="*40)
+for i in range (1,6):
+    for j in range (i):
+        print("*",end='')
+    print()
+print("="*40)
+
+for i in range (1,6):     #pola bintang persegi
+    for j in range (1,6):
+        print("*",end='')
+    print()
+
+
